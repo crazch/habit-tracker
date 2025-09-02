f3208e1d1d43da805bfd18ee6b6d585cc5492caa
crazch <urayzainulkawakib26@gmail.com>
2025-09-02 19:29:50 +0700

    create basic JS add task feature

--- src/modules/todo-list/script.js
+++ src/modules/todo-list/script.js
@@ -10,6 +10,7 @@ const addTaskPriority = document.querySelector(".add-task-priority");
 
 // Tab Container
 const tabContainer = document.querySelector(".tab-container");
+const taskTab = document.querySelector(".task-tab");
 const listTitle = document.querySelector(".list-title");
 const totalTask = document.querySelector(".total-task");
 
@@ -27,4 +28,61 @@ const editorTaskTitle = document.getElementById("editor-task-title");
 const textEditor = document.querySelector(".text-editor-area");
 
 const taskCreationDate = document.querySelector(".created-by");
-const taskLastEditDate = document.querySelector(".last-edited")
+const taskLastEditDate = document.querySelector(".last-edited");
+
+const taskData = JSON.parse(localStorage.getItem("tasks")) || [];
+const currentTask = {};
+
+const removeSpecialChars = (val) => {
+  return val.trim().replace(/[^A-Za-z0-9\-\s]/g, "");
+};
+
+const addTask = () => {
+  const taskObj = {
+    id: `${removeSpecialChars(addTaskInput.value)
+      .toLowerCase()
+      .split(" ")
+      .join("-")}-${Date.now()}`,
+    title: `${removeSpecialChars(addTaskInput.value)}`,
+    date: ``,
+    description: "",
+  };
+
+  taskData.push(taskObj);
+  localStorage.setItem("tasks", JSON.stringify(taskData));
+  renderTaskList();
+};
+
+const renderTaskList = () => {
+  tabContainer.innerHTML = "";
+
+  taskData.forEach(({ id, title, date, description }) => {
+    const taskDiv = document.createElement("div");
+    taskDiv.className = "task-tab";
+    taskDiv.id = id;
+
+    taskDiv.innerHTML = `
+    <div class="task-content">
+      <label>
+        <input type="checkbox" />
+        <span class="task-title">${title}</span>
+      </label>
+      <span class="task-time">${date}</span>
+    </div>
+  `;
+
+    // Attach event listener here
+    taskDiv.addEventListener("click", () => {
+        // SHOW hidden editor
+        // DISPLAY Task Details in Editor
+    });
+
+    tabContainer.appendChild(taskDiv);
+  });
+};
+
+addTaskInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    addTask();
+  }
+});
