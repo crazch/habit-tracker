ece0acd6dad6e1035059aab4cfbdceb7fcfffce0
crazch <urayzainulkawakib26@gmail.com>
2025-09-02 10:08:23 +0700

    add javascript DOM element with querySelector and getElementById

---
+++ src/modules/todo-list/script.js
@@ -0,0 +1,30 @@
+// Header
+const menuBtn = document.querySelector(".menu-btn");
+const pageTitle = document.querySelector(".page-title");
+const moreBtn = document.querySelector(".more-btn");
+
+// Add Task Input
+const addTaskInput = document.getElementById("add-task");
+const addTaskDateBtn = document.querySelector(".add-task-date");
+const addTaskPriority = document.querySelector(".add-task-priority");
+
+// Tab Container
+const tabContainer = document.querySelector(".tab-container");
+const listTitle = document.querySelector(".list-title");
+const totalTask = document.querySelector(".total-task");
+
+// Task Tab
+const taskTabCheckbox = document.getElementById("task-tab-checkbox");
+const taskTabTitle = document.querySelector(".task-title");
+const taskTabTime = document.querySelector(".task-time");
+
+// Task Editor Panel
+const taskEditorContainer = document.querySelector(".task-editor-container");
+const editorCheckbox = document.getElementById("task-complete");
+const editorDateBtn = document.querySelector(".date-btn");
+const priorityEditorBtn = document.querySelector("priority-btn");
+const editorTaskTitle = document.getElementById("editor-task-title");
+const textEditor = document.querySelector(".text-editor-area");
+
+const taskCreationDate = document.querySelector(".created-by");
+const taskLastEditDate = document.querySelector(".last-edited")
